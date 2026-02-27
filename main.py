071e578d747e231d4cff03421178adbb9f95dff0
23f1000184 <23f1000184@ds.study.iitm.ac.in>
2026-02-27 19:41:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# Allow requests from anywhere (needed for graders)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class CommentRequest(BaseModel):
     comment: str
@@ -19,9 +29,7 @@ async def analyze_comment(req: CommentRequest):
 
     if any(word in text for word in positive_words):
         return {"sentiment": "positive", "rating": 4}
-
     elif any(word in text for word in negative_words):
         return {"sentiment": "negative", "rating": 2}
-
     else:
         return {"sentiment": "neutral", "rating": 3}
